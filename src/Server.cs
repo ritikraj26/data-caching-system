085d019c54858a2ebcde2d5fc9973cf4aae366d9
Ritik Raj <ritik@air.local>
2024-09-30 19:47:02 +0530

    Handle concurrent clients

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // Console.WriteLine("Logs from your program will appear here!");
@@ -10,17 +11,23 @@ TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 
 // creating a new web socket connection
-var clientSocket = server.AcceptSocket(); // wait for client
-const string responseString = "+PONG\r\n";
-while(clientSocket.Connected){
-    // for request
-    var buffer = new byte[1024];
-    await clientSocket.ReceiveAsync(buffer);
-
-    // for response
-    var response = Encoding.UTF8.GetBytes(responseString);
-    clientSocket.Send(response);
+while(true){
+    var clientSocket = server.AcceptSocket(); // wait for client
+    _ = HandleClient(clientSocket);
 }
 
-clientSocket.Close();
+async Task HandleClient(Socket clientSocket){
+    const string responseString = "+PONG\r\n";
+    while(clientSocket.Connected){
+        // for request
+        var buffer = new byte[1024];
+        await clientSocket.ReceiveAsync(buffer);
+
+        // for response
+        var response = Encoding.UTF8.GetBytes(responseString);
+        clientSocket.Send(response);
+    }
+
+    clientSocket.Close();
+}
 server.Stop();
